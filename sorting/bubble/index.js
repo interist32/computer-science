db487cb2eac9bea4c25f5d9b69f913e6960321f9
MINSK\Vladimir_Makushev <vladimir_makushev@epam.com>
2017-05-04 19:30:30 +0300

    Bubble sorting optimization

--- sorting/bubble/index.js
+++ sorting/bubble/index.js
@@ -2,7 +2,7 @@ module.exports = bubbleSort;
 
 function bubbleSort(source) {
     for (let j = 0; j < source.length; j++) {
-        for (let i = 0; i < source.length; i++) {
+        for (let i = 0; i < source.length-j; i++) {
             if (source[i] > source[i + 1]) {
                 const buf = source[i + 1];
                 source[i + 1] = source[i];
