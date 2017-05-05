17522edb98df29bfb0fafecc481161590215e63b
MINSK\Vladimir_Makushev <vladimir_makushev@epam.com>
2017-05-05 14:15:11 +0300

    Bubble sort optimization: added flag for preventing redundant iterations if array already sorted

--- sorting/bubble/index.js
+++ sorting/bubble/index.js
@@ -2,12 +2,17 @@ module.exports = bubbleSort;
 
 function bubbleSort(source) {
     for (let j = 0; j < source.length; j++) {
-        for (let i = 0; i < source.length-j; i++) {
+        let isSwapPerformed = false;
+        for (let i = 0; i < source.length - j; i++) {
             if (source[i] > source[i + 1]) {
                 const buf = source[i + 1];
                 source[i + 1] = source[i];
                 source[i] = buf;
+                isSwapPerformed = true;
             }
+        }
+        if (!isSwapPerformed) {
+            break;
         }
     }
     return source;
